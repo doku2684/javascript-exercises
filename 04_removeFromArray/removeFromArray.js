20974f590870b9c608e2c52b564d6ebf5cac440d
doku2684 <joonkim2684@gmail.com>
2023-11-14 22:08:26 +0900

    solve 04-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for (e of elements) {
+        array = array.filter(item => item !== e)
+    }
+    return array
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (typeof x == 'number' && typeof y == 'number') {
+        if (x <= 0 || y <= 0) return "ERROR"
+        if (x > y) {
+            return (x*(x+1)/2 - y*(y-1)/2)
+        } else {
+            return (y*(y+1)/2 - x*(x-1)/2)
+        }
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
